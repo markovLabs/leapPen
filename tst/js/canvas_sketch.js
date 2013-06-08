1cbb89cf9c9169aea3a5a22f71013bd69f5f8855
Victor Regalado <vrrs0988@gmail.com>
2013-06-07 23:43:50 -0400

    track tool  successfully.

--- tst/js/canvas_sketch.js
+++ tst/js/canvas_sketch.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
 	var marker=$('#simple_sketch_mrkv').Sketch_mrkv();
 	marker.startPainting();
-	for(i=0;i<100;i++){
+	for(i=0;i<20;i++){
 		marker.draw(i,i);
 	}
+	var controller = new Leap.Controller({enableGestures: true});
+    controller.loop(function(frame) {
+		siz=frame.tools.length;
+		if(siz>0){
+			for(i=0;i<siz;i++){
+				tool=frame.tools[i];
+				p=tool.tipPosition;
+				marker.draw(p.x,p.y);
+				console.log('('+p.x+','+p.y+')');
+			}			
+		}
+	});
 }); 
